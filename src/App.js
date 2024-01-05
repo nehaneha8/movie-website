a3f91fabd1d0cbd7a5a3bb42d1966662b554065c
nehaneha8 <100853966+nehaneha8@users.noreply.github.com>
2024-01-05 15:47:53 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+
+import MovieCard from "./MovieCard";
+
+import './App.css';
+import SearchIcon from './search.svg';
+const API_URL = 'http://www.omdbapi.com?apikey=851c1a14'
+
+const movie1 = {
+    "Title": "Spiderman",
+    "Year": "1990",
+    "imdbID": "tt0100669",
+    "Type": "movie",
+    "Poster": "N/A"
+}
+
+const App = () => {
+    const [movies, setMovies] = useState([]);
+
+    const [searchTerm, setSearchTerm] = useState([]);
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+
+    }
+    useEffect(()=> {
+
+        searchMovies('spiderman')
+
+    }, []);
+    return (
+        <div className ="app">
+            <h1>The MOVIEVERSE</h1>
+
+            <div className="search"> 
+                <input
+                placeholder = "Search for movies.."
+                value={searchTerm}
+                onChange={(e)=>setSearchTerm(e.target.value)}
+                // onKeyDown={(e) => {searchMovies(searchTerm)}
+                onKeyDown={(e) => {
+                    if (e.key === "Enter")
+                        {searchMovies(searchTerm)};
+                    }}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                />
+            </div>
+
+            {
+            movies?.length > 0
+                ?   
+                (
+                    <div className = "container">
+                        {movies.map((movie) => (
+                            <MovieCard movie={(movie)}/>
+                        ))}
+                    </div>
+                ) :  
+                (
+                    <div className ="empty">
+                        <h2>No movies found</h2>
+                    </div>
+
+                )
+            }
+
+            
+        </div>
+    );
+}
+
+export default App;
